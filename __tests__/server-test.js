729916df5ad6ac7326dfb6445ff1f36154160bd2
deliacodes <deliacodes@gmail.com>
2018-02-20 21:30:22 -0800

    "First failing test."

---
+++ __tests__/server-test.js
@@ -0,0 +1,7 @@
+const getSomething = require('../server');
+
+describe('Bunny', () => {
+  test('Id est', () => {
+    expect(getSomething).toBe(false);
+  });
+});
--- server.js
+++ server.js
@@ -1,6 +1,15 @@
+"use strict";
+
 const express = require('express');
 
 const app = express();
 
 app.use(express.static('public'));
 app.listen(process.env.PORT || 8060);
+
+
+function getSomething() {
+  return true;
+}
+
+module.exports = getSomething;
