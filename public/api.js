b7659dca1461430a2e2f4c159f2861c14b47019f
deliacodes <deliacodes@gmail.com>
2018-02-21 22:12:41 -0800

    Created basic meal, displayed to user, and corrected script issue.

--- public/api.js
+++ public/api.js
@@ -3,12 +3,15 @@
 const createMeals = () => {
   return {
     name: 'Macaroni and Cheese',
-    ingredients: ['macaroni', 'cheese', 'water', 'seasoning', 'salt', 'oil'],
+    ingredients: ['macaroni', ' cheese', ' water', ' seasoning', ' salt', ' oil'],
 
   };
 };
 
 // display created data to the currentMeals div
+
+document.getElementById("currentMeals").textContent = `Name: ${createMeals().name} Ingredients: ${createMeals().ingredients}`;
+
 
 // accept data from the form and then add it to display data
 
