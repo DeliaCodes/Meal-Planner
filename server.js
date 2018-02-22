89b61b5ec34f94c5296c5cbe9f62d4e0d0410931
deliacodes <deliacodes@gmail.com>
2018-02-21 21:15:43 -0800

    Serves HTML page.

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
-"use strict";
-
 const express = require('express');
 
 const app = express();
 
 app.use(express.static('public'));
-app.listen(process.env.PORT || 8060);
 
+const runServer = () => {
+  app.listen(process.env.PORT || 8060);
+  console.log('Server running.');
+};
 
 function getSomething() {
   return true;
 }
 
 module.exports = getSomething;
+
+runServer();
