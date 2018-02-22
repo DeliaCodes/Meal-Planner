88e432cb52387924da7a08903454c4ab18813c1d
deliacodes <deliacodes@gmail.com>
2018-02-21 21:47:00 -0800

    First passing test!!!!!

--- __tests__/api-test.js
+++ __tests__/api-test.js
@@ -2,6 +2,10 @@ const api = require('../public/api');
 
 describe('Kitten!!!!', () => {
   test('Data is created as expected', () => {
-    expect('kitten').toEqual('grey');
+    expect(api).toEqual({
+      name: 'Macaroni and Cheese',
+      ingredients: ['macaroni', 'cheese', 'water', 'seasoning', 'salt', 'oil'],
+
+    });
   });
 });
--- public/api.js
+++ public/api.js
@@ -1,5 +1,15 @@
 // create some data
 
+const createMeals = () => {
+  return {
+    name: 'Macaroni and Cheese',
+    ingredients: ['macaroni', 'cheese', 'water', 'seasoning', 'salt', 'oil'],
+
+  };
+};
+
 // display created data to the currentMeals div
 
 // accept data from the form and then add it to display data
+
+module.exports = createMeals();
