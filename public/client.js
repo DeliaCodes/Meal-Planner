f0b11dbdb2d437b9053fc74fb5778122ac242d11
deliacodes <deliacodes@gmail.com>
2018-02-24 19:04:50 -0800

    Imported get and post functions. Roughed out render and template.

--- public/client.js
+++ public/client.js
@@ -1,3 +1,26 @@
 // add a state variable to store meals
+const mealsAPI = require('./api.js');
 
-document.getElementById("currentMeals").textContent = `Name: ${createMeals().name} Ingredients: ${createMeals().ingredients}`;
+const getMyMeals = mealsAPI.getMeals;
+
+const addMyMeal = mealsAPI.addMeal;
+
+const STORE = {};
+
+// create template for the insertion
+const template = item => `<p>Name: ${item.name}</p> <p>Ingredients: ${item.ingredients}</p>`;
+
+// add some data to the store variable
+const data = {
+  name: 'Macaroni and Cheese',
+  ingredients: ['macaroni', ' cheese', ' water', ' seasoning', ' salt', ' oil'],
+
+};
+
+// render that data to the html
+const render = () => document.getElementById('currentMeals').textContent(template(STORE.meals));
+
+
+// accept data from the form and then add it to display data
+
+// rerender the form
