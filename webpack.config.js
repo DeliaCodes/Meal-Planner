e1e3eedfbbd74af655c78d45f2d565983c3ef010
deliacodes <deliacodes@gmail.com>
2018-03-05 23:50:56 -0800

    Added development mode.

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
     client: './src/client.js',
     // api: './src/api.js',
   },
+  mode: 'development',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
