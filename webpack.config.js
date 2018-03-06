1d351233d6ed9ae9043b326ef9702f519356a3ae
deliacodes <deliacodes@gmail.com>
2018-03-05 21:16:13 -0800

    small edit to entries.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,8 @@
 const path = require('path');
 
 module.exports = {
-  entry: {
-    './src/client.js',
-    './src/api.js',
-  },
+  entry: './src/client.js',
+  entry: './src/api.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
