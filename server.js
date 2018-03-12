72f09f494ea663a43dab14e71ed50e2f19646f83
deliacodes <deliacodes@gmail.com>
2018-03-11 19:36:18 -0700

    Clean up of Server and Package json.

--- server.js
+++ server.js
@@ -4,28 +4,8 @@ const path = require('path');
 
 const app = express();
 
-// const publicPath = path.resolve(__dirname, 'dist');
-
-// use this or the thing below
-// app.use(express.static(publicPath));
-// app.use(express.static('dist'));
-
 app.use(express.static('dist'));
 
-/* eslint-disable */
-/* app.get('*',
-  function (req, res) {
-    /*  const options = {
-       headers: {
-         'content-type': 'application/javascript'
-       }
-     }; */
-// res.sendFile(path.join(__dirname, 'dist/main.css'));
-// res.sendFile(path.join(__dirname, '/dist/bundle.js'), options);
-// res.sendFile(path.join('dist', '/dist/index.html'));
-// res.sendFile(publicPath);
-
-// }); */
 const runServer = (port) => {
   app.listen(process.env.PORT || port);
   console.log('Server running on ', port);
