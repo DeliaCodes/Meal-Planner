c1075a9e17c71f1f2e2a1ea03482582b883a81ba
deliacodes <deliacodes@gmail.com>
2018-03-11 19:36:58 -0700

    Cleanup of Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,5 @@
 const path = require('path');
 
-/* const webpack = require('webpack');
-
-
-module.exports = {
-  entry: {
-    client: './src/client.js',
-    // api: './src/api.js',
-  },
-  mode: 'development',
-  output: {
-    filename: 'bundle.js',
-    path: path.join(__dirname, 'dist'),
-    publicPath: '/',
-  },
-}; */
-
 module.exports = {
   entry: {
     client: './src/client.js',
