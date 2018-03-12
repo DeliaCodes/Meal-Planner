1db82986491a49c11ebe5017666b850737b628a4
deliacodes <deliacodes@gmail.com>
2018-03-11 21:41:30 -0700

    Creating app.js to make testing easier.

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-const express = require('express');
-
-const path = require('path');
-
-const app = express();
+const {
+  app,
+  express,
+} = require('./app.js');
 
 app.use(express.static('dist'));
 
