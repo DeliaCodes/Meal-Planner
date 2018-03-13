8c4c67bf4c36846479bc6fd848ea8877360f686e
deliacodes <deliacodes@gmail.com>
2018-03-12 21:57:08 -0700

    First attempt at passing test. Still failing.

--- src/app.js
+++ src/app.js
@@ -3,6 +3,12 @@ const express = require('express');
 // const path = require('path');
 
 const app = express();
+// const router = express.Router();
+
+app.get('/', (req, res) => {
+  // res.send('Working!!!');
+  res.sendStatus(200);
+});
 
 module.exports = {
   app,
