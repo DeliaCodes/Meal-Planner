3dbae0853483eb22cf2d4788868ef1f5baa46923
deliacodes <deliacodes@gmail.com>
2018-03-17 23:16:57 -0700

    Preparing for generator setup.

--- src/server.js
+++
@@ -1,9 +0,0 @@
-const {
-  app,
-} = require('./app.js');
-
-const port = process.env.PORT || 8010;
-
-app.listen(port);
-
-console.log('Server running on ', port);
--- teardown.js
+++
@@ -1,5 +0,0 @@
-// why does this need to be in a separare file?
-
-module.exports = async function () {
-  await global.__MONGOD__.stop();
-};
