edf54f1e2d7605b162849dc4fb0d8c3e75acb888
deliacodes <deliacodes@gmail.com>
2018-03-23 20:55:00 -0700

    First version of data formatting for db function.

--- src/app.js
+++ src/app.js
@@ -19,6 +19,7 @@ const stringCheck = (mightBeAString) => {
   };
 };
 
+// do I need this and the stringCheck?
 const hasTitleAndIngredient = (userContent) => {
   if (userContent.title !== undefined && userContent.ingredients !== undefined) {
     return userContent;
@@ -26,15 +27,23 @@ const hasTitleAndIngredient = (userContent) => {
     return console.error(userContent, 'needs to be entered');
   }
 };
-
+// need to do this for each value of the request body
 const validateUserInput = (input) => {
   hasTitleAndIngredient(input).then(stringCheck(input)).then(Promise.resolve(input));
-  // check if a meal has a title
-  // check if a meal has an ingredient
-  // check if meal title is a string
-  // check if ingredient is a string
 };
 
+// /\ write tests for the above and below \/
+
+// write something that will convert user input into what is needed by the database
+// take input and convert it into an object with name: "" and ingredients: []
+const convertInputDataIntoDbFormat = (validateUserInput, userData) => {
+  const formattedData = {
+    ingredients: [],
+  };
+  validateUserInput(userData.name).then(formattedData.name = userData.name);
+  validateUserInput(userData.ingredient).then(formattedData.ingredients.push(userData.ingredient));
+  return formattedData;
+};
 
 app.get('/', (req, res) => {
   // res.send('Working!!!');
