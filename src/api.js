f4fd5ada588e7bde3e4df04c128a44cf7806e3a2
deliacodes <deliacodes@gmail.com>
2018-03-23 22:38:01 -0700

    commenting out database code.

--- src/api.js
+++ src/api.js
@@ -9,7 +9,7 @@ const cooperDB = [{
   },
 ];
 
-require('dotenv').config();
+// require('dotenv').config();
 
 const mongoose = require('mongoose');
 
@@ -18,14 +18,14 @@ const {
 } = require('./models.js');
 
 /* global SECRET_DB_USER */
-const dbAddress = `mongodb://${process.env.SECRET_DB_USER}:${process.env.SECRET_DB_PS}@ds113849.mlab.com:13849/mplannerdb`;
+/* const dbAddress = `mongodb://${process.env.SECRET_DB_USER}:${process.env.SECRET_DB_PS}@ds113849.mlab.com:13849/mplannerdb`;
 
 mongoose.connect(dbAddress);
 
-const db = mongoose.connection;
+const db = mongoose.connection; */
 
 // is there a way to do this without a console statement?
-db.on('error', console.error.bind(console, 'MongoDB econnection error:'));
+// db.on('error', console.error.bind(console, 'MongoDB econnection error:'));
 
 // stub post function to add itens to fake database
 const addMealToDB = post => mealModel.create(post);
