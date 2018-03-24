6346986a9503d906364c9a6d5dd9ed14d5b2caba
deliacodes <deliacodes@gmail.com>
2018-03-23 20:39:47 -0700

    First business logic draft - validation of user input.

--- src/app.js
+++ src/app.js
@@ -10,6 +10,32 @@ const {
 
 app.use(express.static('dist'));
 
+// should these be a try and catch block instead of an if / else?
+const stringCheck = (mightBeAString) => {
+  if (typeof mightBeAString === string) {
+    return mightBeAString;
+  } else {
+    return console.error(mightBeAString, 'Is Not A String');
+  };
+};
+
+const hasTitleAndIngredient = (userContent) => {
+  if (userContent.title !== undefined && userContent.ingredients !== undefined) {
+    return userContent;
+  } else {
+    return console.error(userContent, 'needs to be entered');
+  }
+};
+
+const validateUserInput = (input) => {
+  hasTitleAndIngredient(input).then(stringCheck(input)).then(Promise.resolve(input));
+  // check if a meal has a title
+  // check if a meal has an ingredient
+  // check if meal title is a string
+  // check if ingredient is a string
+};
+
+
 app.get('/', (req, res) => {
   // res.send('Working!!!');
   res.sendStatus(200);
