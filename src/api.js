1ab052302e4fb5e3eec5007facb6c40a2cce5fac
deliacodes <deliacodes@gmail.com>
2018-03-25 12:40:49 -0700

    Removing db code from api and adding code splitting comments.

--- src/api.js
+++ src/api.js
@@ -1,3 +1,5 @@
+// this file is a layer between the database, the models, and the app aka endpoints
+
 // creating a fake database to simulate later use
 const cooperDB = [{
     name: 'Macaroni and Cheese',
@@ -16,16 +18,6 @@ const mongoose = require('mongoose');
 const {
   mealModel,
 } = require('./models.js');
-
-/* global SECRET_DB_USER */
-/* const dbAddress = `mongodb://${process.env.SECRET_DB_USER}:${process.env.SECRET_DB_PS}@ds113849.mlab.com:13849/mplannerdb`;
-
-mongoose.connect(dbAddress);
-
-const db = mongoose.connection; */
-
-// is there a way to do this without a console statement?
-// db.on('error', console.error.bind(console, 'MongoDB econnection error:'));
 
 // stub post function to add itens to fake database
 const addMealToDB = post => mealModel.create(post);
