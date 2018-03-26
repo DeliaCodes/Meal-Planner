8592c4bb8dcdf5407eac9ea516deb9168530d517
deliacodes <deliacodes@gmail.com>
2018-03-26 02:53:18 -0700

    adding run server and close server functions.

--- src/server.js
+++ src/server.js
@@ -1,11 +1,52 @@
+/* import {
+  resolve
+} from 'path'; */
+
 // file for the application server
 
 const {
   app,
 } = require('./app.js');
 
-const port = process.env.PORT || 8010;
+let server;
 
-app.listen(port);
+/* const port = process.env.PORT || 8010; */
 
-console.log('Server running on ', port);
+/* app.listen(port); */
+
+/* console.log('Server running on ', port); */
+
+const runServer = () => {
+  const port = process.env.PORT || 8010;
+  return new Promise((resolve, reject) => {
+    server = app.listen(port, () => {
+      console.log(`Your app is listening on port ${port}`);
+      resolve(server);
+    }).on('error', (err) => {
+      reject(err);
+    });
+  });
+};
+
+const closeServer = () => {
+  return new Promise((resolve, reject) => {
+    console.log('Closing server');
+    server.close((err) => {
+      if (err) {
+        reject(err);
+        // so we don't also call `resolve()`
+        return;
+      }
+      resolve();
+    });
+  });
+};
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+}
+
+module.exports = {
+  runServer,
+  closeServer,
+};
