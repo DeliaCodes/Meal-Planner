5ca742a6c3e1e416c8904fe52f677dc8cd016009
deliacodes <deliacodes@gmail.com>
2018-03-26 21:59:23 -0700

    First passing api test.

--- __tests__/api-test.js
+++ __tests__/api-test.js
@@ -1,11 +1,17 @@
-const api = require('../src/api');
+const {
+  getAllMealsFromDB,
+  addMealToDB,
+} = require('../src/api');
 
-xdescribe('Kitten!!!!', () => {
+describe('Database Api works as expected', () => {
   test('Data is created as expected', () => {
-    expect(api).toEqual({
+    const input = {
       name: 'Macaroni and Cheese',
       ingredients: ['macaroni', 'cheese', 'water', 'seasoning', 'salt', 'oil'],
-
+    };
+    expect.assertions(1);
+    return addMealToDB(input).then((data) => {
+      expect(data).toBe(input);
     });
   });
 });
