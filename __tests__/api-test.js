d1c59c5f9ec65f2bb5caee7ec42c78bff732f3d2
Maxim Filimonov <tpaktopsp@gmail.com>
2018-03-28 10:49:01 +0800

    Mongo won't connect magically

--- __tests__/api-test.js
+++ __tests__/api-test.js
@@ -1,17 +1,31 @@
-const {
-  getAllMealsFromDB,
-  addMealToDB,
-} = require('../src/api');
+import mongoose from 'mongoose';
+import { TEST_DATABASE_URL } from '../config';
+
+const { getAllMealsFromDB, addMealToDB } = require('../src/api');
 
 describe('Database Api works as expected', () => {
+  let connection;
+  beforeAll(() => {
+    connection = mongoose.connect(TEST_DATABASE_URL);
+    return connection;
+  });
+  beforeEach(() => {
+    mongoose.connection.db.dropDatabase();
+  });
+
+  afterAll(() => {
+    mongoose.disconnect();
+  });
+
   test('Data is created as expected', () => {
     const input = {
       name: 'Macaroni and Cheese',
       ingredients: ['macaroni', 'cheese', 'water', 'seasoning', 'salt', 'oil'],
     };
-    expect.assertions(1);
+    // expect.assertions(2);
     return addMealToDB(input).then((data) => {
       expect(data).toBe(input);
+      expect(data.id).toBeDefined();
     });
   });
 });
