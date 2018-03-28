ce86becb774a0ce6479f182355eb46c74bc7a431
deliacodes <deliacodes@gmail.com>
2018-03-27 19:49:31 -0700

    Adding App get in the meal.

--- src/app.js
+++ src/app.js
@@ -25,15 +25,16 @@ app.get('/', (req, res) => {
 });
 
 // add endpoint to get all meals
+app.get('/meals', (req, res) => {
+  getAllMealsFromDB().then(res.status(200).json(Response));
+});
 
 app.post('/meals', (req, res) => {
   const newMeal = {
     name: req.body.name,
     ingredients: req.body.ingredients,
   };
-  addMealToDB(newMeal).then(() => {
-    return res.status(200).json(newMeal);
-  });
+  addMealToDB(newMeal).then(() => res.statusCode(200).json(newMeal));
   /*   addMealToDB(newMeal)
     res.status(200).json(newMeal); */
 });
