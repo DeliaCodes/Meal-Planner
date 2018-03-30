f91cac23c65e2768440f2a4ea3d34de429fd16f4
deliacodes <deliacodes@gmail.com>
2018-03-29 21:45:16 -0700

    First failing endpoint test.

--- __tests__/client-test.js
+++ __tests__/client-test.js
@@ -8,19 +8,28 @@ const {
   template,
   mappingMealsIntoTemplate,
   addToState,
+  getMealsFromEndpoint,
 } = require('../src/client');
 
 jest.mock('../src/api');
 
-describe('Client', () => {
-  it('template returns', () => {
+describe('Client Side tests', () => {
+  describe('client-side to server tests', () => {
+    it('successfully return data from meals endpoint', () => {
+      const result = getMealsFromEndpoint();
+      expect(result.status).toBe(200);
+      expect(result.response.body).toBeDefined();
+    });
+  });
+
+  it('template returns - template function', () => {
     expect(template({
       name: 'macaroni',
       ingredients: ['macaroni', 'salt', 'water'],
     })).toContain('macaroni');
   });
 
-  it('mappingMeals is correct', () => {
+  it('mappingMeals is correct - mappingMealsIntoTemplate function', () => {
     const data = [{
       name: 'macaroni',
       ingredients: ['macaroni', 'salt', 'water'],
@@ -31,7 +40,7 @@ describe('Client', () => {
     expect(result).toContain('macaroni');
   });
 
-  it('It renders', () => {
+  it('It renders - render function', () => {
     document.body.innerHTML =
       '<div><div id="currentMeals"/></div>';
     const duck = {
@@ -46,7 +55,7 @@ describe('Client', () => {
     expect($('#currentMeals').html()).toContain('macaroni');
   });
 
-  it('modifies store', () => {
+  it('modifies store - addToState function', () => {
     const appleCore = {
       name: 'macaroni',
       ingredients: ['macaroni', 'salt', 'water'],
