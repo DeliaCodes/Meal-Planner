07d19e05cb117777a45aa0e97201ca8673a9d5bd
deliacodes <deliacodes@gmail.com>
2018-04-03 22:47:39 -0700

    Adding meal to seed. Requiring mealmodel.

--- src/seed.js
+++ src/seed.js
@@ -1,1 +1,10 @@
 // require model - mealmodel.create and then seeds data once by calling script
+
+const {
+  mealmodel,
+} = require('./models');
+
+const meal = {
+  name: 'Boiled Cabbage',
+  description: ['cabbage', ' water', ' salt', ' pepper'],
+};
