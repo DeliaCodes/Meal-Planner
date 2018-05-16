f28386ee2b861b3840b1f3a8b129cf0587c25694
deliacodes <deliacodes@gmail.com>
2018-05-15 22:17:11 -0700

    Added Edit Meal button.

--- src/client.js
+++ src/client.js
@@ -137,7 +137,7 @@ const hideEverything = () => {
 const scheduleTemplate = meals =>
   `<div class="meal"><p class="name">${meals.name}</p><p class="description">${
     meals.description
-  }</p><a class="edit delete" id="${meals._id}">Delete Meal</a></div>`;
+  }</p><a class="edit delete" id="${meals._id}">Delete Meal</a><a class="edit editMeal">Edit Meal</a></div>`;
 
 const dayTemplate = dayInWeek => `<h2 class="day">${dayInWeek}</h2>`;
 
