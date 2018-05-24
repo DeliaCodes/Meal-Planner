7b34201196f90918a1c69d987a361748c3940dc9
deliacodes <deliacodes@gmail.com>
2018-05-23 22:07:25 -0700

    Failing delete test.

--- __tests__/app-test.js
+++ __tests__/app-test.js
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
-import {TEST_DATABASE_URL,} from '../config';
+import {
+  TEST_DATABASE_URL,
+} from '../config';
 
 
 const request = require('supertest');
@@ -46,55 +48,66 @@ describe('Pure function tests', () => {
 });
 
 describe('Meal Endpoint Tests', () => {
-  beforeAll(() => runServer(TEST_DATABASE_URL));
+      beforeAll(() => runServer(TEST_DATABASE_URL));
 
-  beforeEach(() => mongoose.connection.db.dropDatabase());
+      beforeEach(() => mongoose.connection.db.dropDatabase());
 
-  afterAll(() => closeServer());
+      afterAll(() => closeServer());
 
-  describe('Root Path', () => {
-    test('response to get', () =>
-      request(app)
-        .get('/')
+      describe('Root Path', () => {
+        test('response to get', () =>
+          request(app)
+          .get('/')
+          .then((Response) => {
+            expect(Response.statusCode).toBe(200);
+          }));
+      });
+
+      test('GET - returns meals saved in db', () =>
+        request(app)
+        .get('/meals')
         .then((Response) => {
-          expect(Response.statusCode).toBe(200);
+          expect(Response.status).toBe(200);
+          expect(Response.body).toBeDefined();
         }));
-  });
 
-  test('GET - returns meals saved in db', () =>
-    request(app)
-      .get('/meals')
-      .then((Response) => {
-        expect(Response.status).toBe(200);
-        expect(Response.body).toBeDefined();
-      }));
+      test('POST - returns saved meal', () =>
+        request(app)
+        .post('/meals')
+        .send({
+          name: 'cheeseburger',
+          description: 'cheese, burger, and bun',
+          dayOfWeek: '3',
+        })
+        .then((Response) => {
+          expect(Response.statusCode).toEqual(200);
+          expect(Response.body.name).toEqual('cheeseburger');
+          expect(Response.body.description).toContain('cheese');
+          expect(Response.body.dayOfWeek).toEqual('3');
+        }));
 
-  test('POST - returns saved meal', () =>
-    request(app)
-      .post('/meals')
-      .send({
-        name: 'cheeseburger',
-        description: 'cheese, burger, and bun',
-        dayOfWeek: '3',
-      })
-      .then((Response) => {
-        expect(Response.statusCode).toEqual(200);
-        expect(Response.body.name).toEqual('cheeseburger');
-        expect(Response.body.description).toContain('cheese');
-        expect(Response.body.dayOfWeek).toEqual('3');
-      }));
-
-  test('GET and POST - returns schedule', () => request(app)
-    .post('/meals')
-    .send({
-      name: 'mince meat',
-      description: 'a pie',
-      dayOfWeek: '5',
-    })
-    .then(() =>
-      request(app)
-        .get('/schedule'))
-    .then((Response) => {
-      expect(Response.body.Fri[0].name).toEqual('mince meat');
-    }));
-});
+      test('GET and POST - returns schedule', () => request(app)
+        .post('/meals')
+        .send({
+          name: 'mince meat',
+          description: 'a pie',
+          dayOfWeek: '5',
+        })
+        .then(() =>
+          request(app)
+          .get('/schedule'))
+        .then((Response) => {
+          expect(Response.body.Fri[0].name).toEqual('mince meat');
+        }));
+      test('DELETE', () => request(app)
+        .post('/meals')
+        .send({
+          name: 'jellyfish sautee',
+          description: 'brined',
+          dayOfWeek: '2',
+        })
+        .then((Response) => request(app).delete(Response._id).then((Response) => {
+          expect(Response).toBeDefined();
+          expect(Response.statusCode).toEqual(200);
+        }))
+      });
