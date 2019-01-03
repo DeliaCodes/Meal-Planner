944b07fb1e22220059ba40fee985473efc13031e
deliacodes <deliacodes@gmail.com>
2019-01-03 14:36:24 -0800

    adding userID to some finds.

--- src/data-layer.js
+++ src/data-layer.js
@@ -2,21 +2,23 @@
 
 const mongoose = require('mongoose');
 
-const {
-  mealModel,
-} = require('./models.js');
+const { mealModel } = require('./models.js');
 
 const addMealToDB = mealModel.create.bind(mealModel);
 
 /* pass in user name and default value of null,
 if user name is passed in get only meals with that user */
 
-const getAllMealsFromDB = () => mealModel.find();
+const getAllMealsFromDB = userID => mealModel.find({ userID });
 
-const removeMealFromDB = id => mealModel.deleteOne({
-  _id: id,
-});
+// add userID validation
+const removeMealFromDB = (id, userID) =>
+  mealModel.deleteOne({
+    _id: id,
+    userID,
+  });
 
+// add userID validation to this function by using a find and then a promise to update
 const updateMealInDB = (id, obj) => mealModel.findByIdAndUpdate(id, obj);
 
 // exporting get and post functions.
