d8145e576c8e3560f50ecf08797563340678d8b0
deliacodes <deliacodes@gmail.com>
2019-01-03 14:35:27 -0800

    adding useNewUrlParser

--- src/server.js
+++ src/server.js
@@ -1,9 +1,7 @@
 // file for the application server
 require('dotenv').config();
 
-const {
-  app,
-} = require('./app.js');
+const { app } = require('./app.js');
 
 let server;
 
@@ -11,25 +9,30 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-const {
-  DATABASE_URL,
-} = require('../config');
+const { DATABASE_URL } = require('../config');
 
 const connectMongo = mongoose.connect.bind(mongoose);
-const startNodeServer = port => new Promise((resolve, reject) => {
-  server = app.listen(port, () => {
-    console.log(`Your app is listening on port ${port}`);
-    resolve();
-  })
-    .on('error', (err) => {
-      reject(err);
-    });
-});
+const startNodeServer = port =>
+  new Promise((resolve, reject) => {
+    server = app
+      .listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+      .on('error', (err) => {
+        reject(err);
+      });
+  });
 const disconnectMongo = mongoose.disconnect.bind(mongoose);
 
-function runServer(databaseUrl = DATABASE_URL, port = process.env.PORT || 5000) {
+function runServer(
+  databaseUrl = DATABASE_URL,
+  port = process.env.PORT || 5000,
+) {
   // console.log(databaseUrl);
-  return connectMongo(databaseUrl)
+  return connectMongo(databaseUrl, {
+    useNewUrlParser: true,
+  })
     .then(startNodeServer(port))
     .catch((e) => {
       disconnectMongo();
@@ -37,10 +40,11 @@ function runServer(databaseUrl = DATABASE_URL, port = process.env.PORT || 5000) 
     });
 }
 
-const closeNodeServer = () => new Promise((resolve, reject) => {
-  console.log('Closing server');
-  server.close(err => (err ? reject(err) : resolve()));
-});
+const closeNodeServer = () =>
+  new Promise((resolve, reject) => {
+    console.log('Closing server');
+    server.close(err => (err ? reject(err) : resolve()));
+  });
 
 const closeServer = () => disconnectMongo().then(closeNodeServer);
 
