a4c65eb18ad0e5ff5e86ebba00d5aac4f038cd86
deliacodes <deliacodes@gmail.com>
2019-01-06 18:55:39 -0800

    Formatting changes.

--- src/meals/meals-router.js
+++ src/meals/meals-router.js
@@ -19,45 +19,43 @@
  * @property {Meal[]} Sat - The first day
  */
 
-const {
-    app,
-} = require('../app.js');
+const { app } = require('../app.js');
 
 const {
-    addMealToDB,
-    getAllMealsFromDB,
-    removeMealFromDB,
-    updateMealInDB,
+  addMealToDB,
+  getAllMealsFromDB,
+  removeMealFromDB,
+  updateMealInDB,
 } = require('../data-layer.js');
 
 // add /api to all of these and should be a router
 
 app.get('/api/meals', jwtAuth, (req, res) => {
-    console.log('/meals get');
-    // pass userid into this
-    getAllMealsFromDB().then((meals) => {
-        res.status(200).json(meals);
-    });
+  console.log('/meals get');
+  // pass userid into this
+  getAllMealsFromDB().then((meals) => {
+    res.status(200).json(meals);
+  });
 });
 
 app.post('/api/meals', jwtAuth, (req, res) => {
-    const newMeal = {
-        name: req.body.name,
-        description: req.body.description,
-        dayOfWeek: req.body.dayOfWeek,
-        user: req.user._id,
-    };
-    addMealToDB(newMeal).then(meal => res.status(200).json(meal));
+  const newMeal = {
+    name: req.body.name,
+    description: req.body.description,
+    dayOfWeek: req.body.dayOfWeek,
+    user: req.user._id,
+  };
+  addMealToDB(newMeal).then(meal => res.status(200).json(meal));
 });
 
 app.put('/api/meals/:id', jwtAuth, (req, res) => {
-    const mealToSend = {
-        _id: req.params.id,
-        name: req.body.name,
-        description: req.body.description,
-        dayOfWeek: req.body.dayOfWeek,
-    };
-    updateMealInDB(req.params.id, mealToSend).then(() => res.status(204).end());
+  const mealToSend = {
+    _id: req.params.id,
+    name: req.body.name,
+    description: req.body.description,
+    dayOfWeek: req.body.dayOfWeek,
+  };
+  updateMealInDB(req.params.id, mealToSend).then(() => res.status(204).end());
 });
 
 /**
@@ -65,40 +63,40 @@ app.put('/api/meals/:id', jwtAuth, (req, res) => {
  * @returns {Schedule} sortedItems items sorted
  */
 const sortMealData = (data) => {
-    const sortedItems = {
-        Sun: [],
-        Mon: [],
-        Tue: [],
-        Wed: [],
-        Thu: [],
-        Fri: [],
-        Sat: [],
-    };
+  const sortedItems = {
+    Sun: [],
+    Mon: [],
+    Tue: [],
+    Wed: [],
+    Thu: [],
+    Fri: [],
+    Sat: [],
+  };
 
-    data.forEach(m =>
-        sortedItems[
-            `${moment()
-                .weekday(m.dayOfWeek)
-                .format('ddd')}`
-        ].push(m));
+  data.forEach(m =>
+    sortedItems[
+      `${moment()
+        .weekday(m.dayOfWeek)
+        .format('ddd')}`
+    ].push(m),);
 
-    return sortedItems;
+  return sortedItems;
 };
 
 app.get('/api/schedule', jwtAuth, (req, res) => {
-    // add user paramter to getAllMeals
-    getAllMealsFromDB().then((meals) => {
-        console.log(meals);
-        const scheduledMeals = sortMealData(meals);
-        return res.status(200).json(scheduledMeals);
-    });
+  // add user paramter to getAllMeals
+  getAllMealsFromDB().then((meals) => {
+    console.log(meals);
+    const scheduledMeals = sortMealData(meals);
+    return res.status(200).json(scheduledMeals);
+  });
 });
 
 app.delete('/api/meals/:id', jwtAuth, (req, res) => {
-    removeMealFromDB(req.params.id).then(() => res.status(204).end());
+  removeMealFromDB(req.params.id).then(() => res.status(204).end());
 });
 
 module.exports = {
-    app,
-    sortMealData,
+  app,
+  sortMealData,
 };
--- src/users/user-router.js
+++ src/users/user-router.js
@@ -17,14 +17,12 @@ router.post('/register', jsonParser, (req, res) => {
       code: 422,
       reason: 'ValidationError',
       message: 'Missing field',
-      location: missingField
+      location: missingField,
     });
   }
 
   const stringFields = ['username', 'password', 'firstName', 'lastName'];
-  const nonStringField = stringFields.find(
-    field => field in req.body && typeof req.body[field] !== 'string'
-  );
+  const nonStringField = stringFields.find(field => field in req.body && typeof req.body[field] !== 'string',);
 
   if (nonStringField) {
     return res.status(422).json({
@@ -43,9 +41,7 @@ router.post('/register', jsonParser, (req, res) => {
   // We'll silently trim the other fields, because they aren't credentials used
   // to log in, so it's less of a problem.
   const explicityTrimmedFields = ['username', 'password'];
-  const nonTrimmedField = explicityTrimmedFields.find(
-    field => req.body[field].trim() !== req.body[field]
-  );
+  const nonTrimmedField = explicityTrimmedFields.find(field => req.body[field].trim() !== req.body[field],);
 
   if (nonTrimmedField) {
     return res.status(422).json({
@@ -65,33 +61,29 @@ router.post('/register', jsonParser, (req, res) => {
       // bcrypt truncates after 72 characters, so let's not give the illusion
       // of security by storing extra (unused) info
       max: 72,
-    }
+    },
   };
-  const tooSmallField = Object.keys(sizedFields).find(
-    field =>
+  const tooSmallField = Object.keys(sizedFields).find(field =>
       'min' in sizedFields[field] &&
-      req.body[field].trim().length < sizedFields[field].min
-  );
-  const tooLargeField = Object.keys(sizedFields).find(
-    field =>
+      req.body[field].trim().length < sizedFields[field].min,);
+  const tooLargeField = Object.keys(sizedFields).find(field =>
       'max' in sizedFields[field] &&
-      req.body[field].trim().length > sizedFields[field].max
-  );
+      req.body[field].trim().length > sizedFields[field].max,);
 
   if (tooSmallField || tooLargeField) {
     return res.status(422).json({
       code: 422,
       reason: 'ValidationError',
       message: tooSmallField
-        ? `Must be at least ${sizedFields[tooSmallField]
-          .min} characters long`
-        : `Must be at most ${sizedFields[tooLargeField]
-          .max} characters long`,
-      location: tooSmallField || tooLargeField
+        ? `Must be at least ${sizedFields[tooSmallField].min} characters long`
+        : `Must be at most ${sizedFields[tooLargeField].max} characters long`,
+      location: tooSmallField || tooLargeField,
     });
   }
 
-  let { username, password, firstName = '', lastName = '' } = req.body;
+  let {
+ username, password, firstName = '', lastName = '' 
+} = req.body;
   // Username and password come in pre-trimmed, otherwise we throw an error
   // before this
   firstName = firstName.trim();
@@ -106,23 +98,20 @@ router.post('/register', jsonParser, (req, res) => {
           code: 422,
           reason: 'ValidationError',
           message: 'Username already taken',
-          location: 'username'
+          location: 'username',
         });
       }
       // If there is no existing user, hash the password
       return User.hashPassword(password);
     })
-    .then((hash) => {
-      return User.create({
+    .then(hash =>
+      User.create({
         username,
         password: hash,
         firstName,
         lastName,
-      });
-    })
-    .then((user) => {
-      return res.status(201).json(user.serialize());
-    })
+      }),)
+    .then(user => res.status(201).json(user.serialize()))
     .catch((err) => {
       // Forward validation errors on to the client, otherwise give a 500
       // error because something unexpected has happened
